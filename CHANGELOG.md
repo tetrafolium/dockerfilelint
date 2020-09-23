1c9e1cbc2a89435717e867b723b82043d16df7c5
Inspecode <inspecode-bot@rocro.com>
2020-09-23 05:37:10 +0000

    Fix Prettier errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,55 +3,46 @@
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
 <a name="1.5.0"></a>
+
 # [1.5.0](https://github.com/replicatedhq/dockerfilelint/compare/v1.4.0...v1.5.0) (2019-02-04)
 
-
 ### Features
 
-* Allows ARG before FROM ([#105](https://github.com/replicatedhq/dockerfilelint/issues/105)) ([5fa023d](https://github.com/replicatedhq/dockerfilelint/commit/5fa023d))
-
-
+- Allows ARG before FROM ([#105](https://github.com/replicatedhq/dockerfilelint/issues/105)) ([5fa023d](https://github.com/replicatedhq/dockerfilelint/commit/5fa023d))
 
 <a name="1.4.0"></a>
+
 # [1.4.0](https://github.com/replicatedhq/dockerfilelint/compare/v1.3.0...v1.4.0) (2017-09-13)
 
-
 ### Features
 
-* support for multi-stage builds ([#84](https://github.com/replicatedhq/dockerfilelint/issues/84)) ([72d2087](https://github.com/replicatedhq/dockerfilelint/commit/72d2087))
-
-
+- support for multi-stage builds ([#84](https://github.com/replicatedhq/dockerfilelint/issues/84)) ([72d2087](https://github.com/replicatedhq/dockerfilelint/commit/72d2087))
 
 <a name="1.3.0"></a>
+
 # [1.3.0](https://github.com/replicatedhq/dockerfilelint/compare/v1.2.0...v1.3.0) (2017-09-12)
 
-
 ### Bug Fixes
 
-* port/protocol is a valid Dockerfile syntax ([#85](https://github.com/replicatedhq/dockerfilelint/issues/85)) ([1698c61](https://github.com/replicatedhq/dockerfilelint/commit/1698c61))
-
+- port/protocol is a valid Dockerfile syntax ([#85](https://github.com/replicatedhq/dockerfilelint/issues/85)) ([1698c61](https://github.com/replicatedhq/dockerfilelint/commit/1698c61))
 
 ### Features
 
-* add --version cli option ([#87](https://github.com/replicatedhq/dockerfilelint/issues/87)) ([78b58e2](https://github.com/replicatedhq/dockerfilelint/commit/78b58e2))
-
-
+- add --version cli option ([#87](https://github.com/replicatedhq/dockerfilelint/issues/87)) ([78b58e2](https://github.com/replicatedhq/dockerfilelint/commit/78b58e2))
 
 <a name="1.2.0"></a>
-# [1.2.0](https://github.com/replicatedhq/dockerfilelint/compare/v1.1.2...v1.2.0) (2017-07-03)
 
+# [1.2.0](https://github.com/replicatedhq/dockerfilelint/compare/v1.1.2...v1.2.0) (2017-07-03)
 
 ### Features
 
-* cli can now output json with the `--json` flag ([#76](https://github.com/replicatedhq/dockerfilelint/issues/76)) ([c4b13d2](https://github.com/replicatedhq/dockerfilelint/commit/c4b13d2))
-* support for multiple files on command line ([38f4d72](https://github.com/replicatedhq/dockerfilelint/commit/38f4d72))
-
-
+- cli can now output json with the `--json` flag ([#76](https://github.com/replicatedhq/dockerfilelint/issues/76)) ([c4b13d2](https://github.com/replicatedhq/dockerfilelint/commit/c4b13d2))
+- support for multiple files on command line ([38f4d72](https://github.com/replicatedhq/dockerfilelint/commit/38f4d72))
 
 <a name="1.1.2"></a>
-## 1.1.2 (2016-06-18)
 
+## 1.1.2 (2016-06-18)
 
 ### Bug Fixes
 
-* use configFilePath when processing stdin ([3bde4f3](https://github.com/replicatedhq/dockerfilelint/commit/3bde4f3))
+- use configFilePath when processing stdin ([3bde4f3](https://github.com/replicatedhq/dockerfilelint/commit/3bde4f3))
--- test/checks.js
+++ test/checks.js
@@ -1,5 +1,5 @@
-var expect = require('chai').expect
-var checks = require('../lib/checks.js')
+var expect = require("chai").expect;
+var checks = require("../lib/checks.js");
 
 describe("checks", function() {
   describe("#expose_container_port_only(args)", function() {
@@ -7,8 +7,9 @@ describe("checks", function() {
       expect(checks.expose_container_port_only("8000")).to.be.empty;
       expect(checks.expose_container_port_only("8000:8000")).to.have.length(1);
       expect(checks.expose_container_port_only("8000 8001")).to.be.empty;
-      expect(checks.expose_container_port_only("8000 8001:8000"))
-          .to.have.length(1);
+      expect(
+        checks.expose_container_port_only("8000 8001:8000")
+      ).to.have.length(1);
     });
   });
 
@@ -30,7 +31,7 @@ describe("checks", function() {
       expect(checks.label_format("key=value")).to.be.empty;
       expect(checks.label_format("key=value key=value")).to.be.empty;
       expect(checks.label_format("namespace.key=value")).to.be.empty;
-      expect(checks.label_format("key=\"value value\"")).to.be.empty;
+      expect(checks.label_format('key="value value"')).to.be.empty;
       expect(checks.label_format("key=value value")).to.have.length(1);
       expect(checks.label_format("key key=value")).to.have.length(1);
       expect(checks.label_format("key=value\\ value")).to.be.empty;
@@ -59,37 +60,33 @@ describe("checks", function() {
   });
 
   describe("#valid_maintainer(args)", function() {
-    it("validates maintainer command has exactly one maintainer and outputs MAINTAINER is deprecated in 1.13",
-       function() {
-         expect(checks.valid_maintainer("user <test@gmail.com>"))
-             .to.have.length(1)
-             .and.to.eql([ 'deprecated_in_1.13' ]);
-         expect(checks.valid_maintainer("user without email"))
-             .to.have.length(2);
-         expect(checks.valid_maintainer(
-                    "user <test@gmail.com> user2 <test@gmail.com>"))
-             .to.have.length(2);
-       });
+    it("validates maintainer command has exactly one maintainer and outputs MAINTAINER is deprecated in 1.13", function() {
+      expect(checks.valid_maintainer("user <test@gmail.com>"))
+        .to.have.length(1)
+        .and.to.eql(["deprecated_in_1.13"]);
+      expect(checks.valid_maintainer("user without email")).to.have.length(2);
+      expect(
+        checks.valid_maintainer("user <test@gmail.com> user2 <test@gmail.com>")
+      ).to.have.length(2);
+    });
   });
 
   describe("#param_count_min(args, min_count)", function() {
-    it("validates that a argument line, split on whitespace, contains at least min_count elements",
-       function() {
-         expect(checks.param_count_min("one", 1)).to.equal(true);
-         expect(checks.param_count_min("one", 2)).to.equal(false);
-         expect(checks.param_count_min("one two three", 1)).to.equal(true);
-         expect(checks.param_count_min("one two three", 4)).to.equal(false);
-       })
+    it("validates that a argument line, split on whitespace, contains at least min_count elements", function() {
+      expect(checks.param_count_min("one", 1)).to.equal(true);
+      expect(checks.param_count_min("one", 2)).to.equal(false);
+      expect(checks.param_count_min("one two three", 1)).to.equal(true);
+      expect(checks.param_count_min("one two three", 4)).to.equal(false);
+    });
   });
 
   describe("#is_dir_in_context(dir)", function() {
-    it("validates that a directory is relative (to the docker context)",
-       function() {
-         expect(checks.is_dir_in_context("/etc")).to.equal(false);
-         expect(checks.is_dir_in_context("../home")).to.equal(false);
-         expect(checks.is_dir_in_context("./test")).to.equal(true);
-         expect(checks.is_dir_in_context("test")).to.equal(true);
-       })
+    it("validates that a directory is relative (to the docker context)", function() {
+      expect(checks.is_dir_in_context("/etc")).to.equal(false);
+      expect(checks.is_dir_in_context("../home")).to.equal(false);
+      expect(checks.is_dir_in_context("./test")).to.equal(true);
+      expect(checks.is_dir_in_context("test")).to.equal(true);
+    });
   });
 
   describe("#is_valid_add(args)", function() {
@@ -101,30 +98,26 @@ describe("checks", function() {
       expect(checks.is_valid_add("/text test/")).to.have.length(1);
       expect(checks.is_valid_add("test test2 test/")).to.be.empty;
       expect(checks.is_valid_add("test test2 test")).to.have.length(1);
-    })
+    });
   });
 
-  describe(
-      "#is_wrapped_in_quotes(str)",
-      function() {
-        it("returns a bool indicating if there are matching quotes on each side of a string",
-           function() {
-             expect(checks.is_wrapped_in_quotes("test")).to.equal(false);
-             expect(checks.is_wrapped_in_quotes("'tes't")).to.equal(false);
-             expect(checks.is_wrapped_in_quotes("'test'")).to.equal(true);
-             expect(checks.is_wrapped_in_quotes("\"test\"")).to.equal(true);
-           })
-      }),
-
-      describe("#is_valid_workdir(args)", function() {
-        it("validates workdir command is valid", function() {
-          expect(checks.is_valid_workdir("test")).to.be.empty;
-          expect(checks.is_valid_workdir("/test/test")).to.be.empty;
-          expect(checks.is_valid_workdir("test test")).to.have.length(1);
-          expect(checks.is_valid_workdir("\"test test\"")).to.be.empty;
-          expect(checks.is_valid_workdir("'test test'")).to.be.empty;
-        })
+  describe("#is_wrapped_in_quotes(str)", function() {
+    it("returns a bool indicating if there are matching quotes on each side of a string", function() {
+      expect(checks.is_wrapped_in_quotes("test")).to.equal(false);
+      expect(checks.is_wrapped_in_quotes("'tes't")).to.equal(false);
+      expect(checks.is_wrapped_in_quotes("'test'")).to.equal(true);
+      expect(checks.is_wrapped_in_quotes('"test"')).to.equal(true);
+    });
+  }),
+    describe("#is_valid_workdir(args)", function() {
+      it("validates workdir command is valid", function() {
+        expect(checks.is_valid_workdir("test")).to.be.empty;
+        expect(checks.is_valid_workdir("/test/test")).to.be.empty;
+        expect(checks.is_valid_workdir("test test")).to.have.length(1);
+        expect(checks.is_valid_workdir('"test test"')).to.be.empty;
+        expect(checks.is_valid_workdir("'test test'")).to.be.empty;
       });
+    });
 
   describe("#is_valid_env(args)", function() {
     it("validates env command is valid", function() {
@@ -133,6 +126,6 @@ describe("checks", function() {
       expect(checks.is_valid_env("test test")).to.be.empty;
       expect(checks.is_valid_env("test longer value")).to.be.empty;
       expect(checks.is_valid_env("test=test test2=test2")).to.be.empty;
-    })
+    });
   });
 });
